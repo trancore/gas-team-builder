0b4d319206153d9151e1a6675bb1422203c38151
trancore <ki.program0915@gmail.com>
2024-11-28 00:44:06 +0900

    feature: add Header component

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "~/assets/css/reset.css";
 import "~/assets/css/base.css";
+import Header from "~/components/Header/Header.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <header>test</header>
+    <Header />
     <main>content</main>
   </React.StrictMode>,
 );
