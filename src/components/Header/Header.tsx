4b629756cf7a4bd41799a7d17a03068e0a479c43
trancore <ki.program0915@gmail.com>
2024-11-28 00:26:47 +0900

    feature: add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+﻿import styles from "~/components/Header/Header.module.scss";
+import TextForm from "~/components/Form/TextForm.tsx";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.appTitleInput}>
+        <p className={styles.appTitle}>Team Builder</p>
+        <div className={styles.inputBox}>
+          <TextForm />
+          <button>
+            <p>読み込む</p>
+          </button>
+        </div>
+      </div>
+      <button>
+        <p>出力する</p>
+      </button>
+    </header>
+  );
+}
