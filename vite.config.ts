f6052dc8a219a824fb50f74323836a5ebf9ebfea
trancore <ki.program0915@gmail.com>
2024-11-27 22:48:37 +0900

    feature: set path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
   build: {
     outDir: "server/hosting",
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
   },
 });
