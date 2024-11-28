8b367fb4f42684e4adaf970894513a87129ac9f2
trancore <ki.program0915@gmail.com>
2024-11-29 00:48:48 +0900

    feature: add Additional component

---
+++ src/components/Group/AdditionalGroup.tsx
@@ -0,0 +1,15 @@
+﻿import styles from "~/components/Group/AdditionalGroup.module.scss";
+
+export default function AdditionalGroup() {
+  return (
+    <div className={styles.additionalGroup}>
+      <table>
+        <tbody>
+          <tr>
+            <td className={styles.add}>+ 追加</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
