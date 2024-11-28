fc12c4ecedb2a5f75684c52d1f4985abb785f2fd
trancore <ki.program0915@gmail.com>
2024-11-29 00:49:20 +0900

    feature: Tag Component

---
+++ src/components/Tag/Tag.tsx
@@ -0,0 +1,9 @@
+﻿import styles from "~/components/Tag/Tag.module.scss";
+
+type Props = {
+  text: string;
+};
+
+export default function Tag({ text }: Props) {
+  return <div className={styles.tag}>{text}</div>;
+}
