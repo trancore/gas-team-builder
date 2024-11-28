b6ed219f1154673332c5a6eadda1a6cdbc0ed811
trancore <ki.program0915@gmail.com>
2024-11-29 00:49:05 +0900

    feature: add Group component

---
+++ src/components/Group/Group.tsx
@@ -0,0 +1,37 @@
+﻿import styles from "~/components/Group/Group.module.scss";
+
+export default function Group() {
+  return (
+    <div className={styles.group}>
+      <table className={styles.groupTop}>
+        <tbody>
+          <tr>
+            <td>サブタグ１</td>
+          </tr>
+          <tr>
+            <td className={styles.add}>+ 追加</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className={styles.groupMember}>
+        <tbody>
+          <tr>
+            <td>サブタグ１</td>
+          </tr>
+          <tr>
+            <td>サブタグ２</td>
+          </tr>
+          <tr>
+            <td>サブタグ３</td>
+          </tr>
+          <tr>
+            <td>サブタグ４</td>
+          </tr>
+          <tr>
+            <td className={styles.add}>+ 追加</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
