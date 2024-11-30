6f905d9bc3fc70607aacf99a04464fd87a0acc98
trancore <ki.program0915@gmail.com>
2024-11-30 22:32:22 +0900

    feature: add placeholder in TextForm component

--- src/components/Form/TextForm.tsx
+++ src/components/Form/TextForm.tsx
@@ -1,5 +1,11 @@
 ﻿import styles from "~/components/Form/TextForm.module.scss";
 
 export default function TextForm() {
-  return <input type="text" className={styles.input}></input>;
+  return (
+    <input
+      type="text"
+      className={styles.input}
+      placeholder="スプレッドシートのファイルIDを入れてください"
+    ></input>
+  );
 }
