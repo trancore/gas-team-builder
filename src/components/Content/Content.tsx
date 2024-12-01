c77e532282efc3d14a9623c1caeb7b2fbd4404ef
trancore <ki.program0915@gmail.com>
2024-12-01 21:58:21 +0900

    feature: set data

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,22 +2,30 @@
 import Tag from "~/components/Tag/Tag";
 import Group from "~/components/Group/Group";
 import AdditionalGroup from "~/components/Group/AdditionalGroup";
-import { useState } from "react";
+import { ComponentProps, useState } from "react";
+import { DragObjectTag } from "~/types/DragAndDrop";
+
+type GroupType = ComponentProps<typeof Group>["group"];
 
 export default function Content() {
-  const [mainTags] = useState([
+  const [mainTags, setMainTag] = useState<DragObjectTag[]>([
     { id: "1", name: "メインタグ１" },
     { id: "2", name: "メインタグ２" },
     { id: "3", name: "メインタグ３" },
     { id: "4", name: "メインタグ４" },
     { id: "5", name: "メインタグ５" },
   ]);
-  const [subTags] = useState([
+  const [subTags, setSubTag] = useState<DragObjectTag[]>([
     { id: "6", name: "サブタグ６" },
     { id: "7", name: "サブタグ７" },
     { id: "8", name: "サブタグ８" },
     { id: "9", name: "サブタグ９" },
     { id: "10", name: "サブタグ１０" },
+  ]);
+  const [groups, setGroups] = useState<GroupType[]>([
+    { main: {}, sub: [{}] },
+    { main: {}, sub: [{}] },
+    { main: {}, sub: [{}] },
   ]);
   // const { tag, offsetDifference, isDragging } = useDragLayer((monitor) => ({
   //   tag: monitor.getItem() as DragObjectTag,
@@ -34,23 +42,41 @@ export default function Content() {
       <div className={styles.tagBox}>
         <div className={styles.tagMainBox}>
           {mainTags.map((mainTag) => {
-            return <Tag key={mainTag.id} id={mainTag.id} text={mainTag.name} />;
+            return (
+              <Tag
+                key={`main-tag${mainTag.id}`}
+                id={mainTag.id || ""}
+                text={mainTag.name || ""}
+              />
+            );
           })}
         </div>
         <div className={styles.tagSubBox}>
           {subTags.map((subTag) => {
-            return <Tag key={subTag.id} id={subTag.id} text={subTag.name} />;
+            return (
+              <Tag
+                key={`sub-tag${subTag.id}`}
+                id={subTag.id || ""}
+                text={subTag.name || ""}
+              />
+            );
           })}
         </div>
       </div>
       <div className={styles.groupBox}>
-        <Group />
-        <Group />
-        <Group />
-        <Group />
-        <Group />
-        <Group />
-        <AdditionalGroup />
+        {groups.map((group, index) => {
+          return (
+            <Group
+              key={index}
+              componentId={index}
+              group={group}
+              setMainTag={setMainTag}
+              setSubTag={setSubTag}
+              setGroups={setGroups}
+            />
+          );
+        })}
+        <AdditionalGroup setGroups={setGroups} />
       </div>
     </main>
   );
--- src/components/Group/Group.tsx
+++ src/components/Group/Group.tsx
@@ -1,41 +1,67 @@
 ﻿import styles from "~/components/Group/Group.module.scss";
 import HideButton from "~/components/Button/HideButton";
 import { GroupCell } from "~/components/Group/GroupCell";
+import { Dispatch, SetStateAction } from "react";
+import { DragObjectTag } from "~/types/DragAndDrop";
 
-export default function Group() {
+type SetTag = Dispatch<SetStateAction<DragObjectTag[]>>;
+type Group = {
+  main: DragObjectTag;
+  sub: DragObjectTag[];
+};
+
+type Props = {
+  componentId: number;
+  group: Group;
+  setMainTag: SetTag;
+  setSubTag: SetTag;
+  setGroups: Dispatch<SetStateAction<Group[]>>;
+};
+
+export default function Group({
+  componentId,
+  group,
+  setMainTag,
+  setSubTag,
+  setGroups,
+}: Props) {
+  function addSub() {
+    group.sub.push({});
+    const newGroup = group;
+    setGroups((prev) =>
+      prev.map((group, index) => {
+        if (componentId === index) {
+          return newGroup;
+        }
+        return group;
+      }),
+    );
+  }
+  function removeTeams() {
+    setGroups((prev) => [...prev.filter((_, index) => index !== componentId)]);
+  }
+
   return (
     <div className={styles.group}>
       <table className={styles.groupTop}>
         <tbody>
           <tr>
-            <GroupCell text="サブタブ１" />
-          </tr>
-          <tr>
-            <td className={styles.add}>
-              <HideButton>
-                <p>+ 追加</p>
-              </HideButton>
-            </td>
+            <GroupCell text={group.main?.name || ""} setTag={setMainTag} />
           </tr>
         </tbody>
       </table>
       <table className={styles.groupMember}>
         <tbody>
-          <tr>
-            <GroupCell text="サブタブ１" />
-          </tr>
-          <tr>
-            <GroupCell text="サブタブ２" />
-          </tr>
-          <tr>
-            <GroupCell text="サブタブ３" />
-          </tr>
-          <tr>
-            <GroupCell text="サブタブ４" />
-          </tr>
+          {group.sub?.map((tag, index) => {
+            return (
+              <tr key={index}>
+                <GroupCell text={tag.name || ""} setTag={setSubTag} />
+              </tr>
+            );
+          })}
           <tr>
             <td className={styles.add}>
-              <HideButton>
+              <HideButton onClick={addSub}>
                 <p>+ 追加</p>
               </HideButton>
             </td>
@@ -46,7 +72,7 @@ export default function Group() {
         <tbody>
           <tr>
             <td className={styles.add}>
-              <HideButton>
+              <HideButton onClick={removeTeams}>
                 <p>- 削除</p>
               </HideButton>
             </td>
