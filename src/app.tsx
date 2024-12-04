3d72cf8fc98bbe9c36de99589a45f4ebe4606e9f
trancore <ki.program0915@gmail.com>
2024-12-04 18:33:10 +0900

    feature: add App Component

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+﻿import { DndProvider } from "react-dnd/dist/core/DndProvider";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Header from "~/components/Header/Header";
+import Content from "~/components/Content/Content";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <DndProvider backend={HTML5Backend}>
+        <Content />
+      </DndProvider>
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,10 @@ import ReactDOM from "react-dom/client";
 
 import "~/assets/css/reset.css";
 import "~/assets/css/base.css";
-import Header from "~/components/Header/Header";
-import Content from "./components/Content/Content";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import App from "~/app";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Header />
-    <DndProvider backend={HTML5Backend}>
-      <Content />
-    </DndProvider>
+    <App />
   </React.StrictMode>,
 );
