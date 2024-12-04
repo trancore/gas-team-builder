c3626e03f4c374b99f060bcabce6c7dcdf991194
trancore <ki.program0915@gmail.com>
2024-12-04 22:48:47 +0900

    feature: add env type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,11 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_SPREAD_SHEET_NAME: string | undefined;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
 declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
