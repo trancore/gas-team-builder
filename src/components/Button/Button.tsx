4d371ee3b59caa8a92970a18848fc44ca7fda617
trancore <ki.program0915@gmail.com>
2024-12-04 22:46:34 +0900

    feature: add env

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,14 @@
-﻿import styles from "~/components/Button/Button.module.scss";
+﻿import { MouseEventHandler } from "react";
+import styles from "~/components/Button/Button.module.scss";
 
 type Props = {
   text: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
 };
 
-export default function Button({ text }: Props) {
+export default function Button({ text, onClick }: Props) {
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={onClick}>
       <p>{text}</p>
     </button>
   );
